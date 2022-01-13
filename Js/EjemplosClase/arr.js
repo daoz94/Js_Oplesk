15036a8a9b8c2a5fe6cffe81896b9af3e3dede26
Daniel Ospino <daoz1994@gmail.com>
2022-01-13 16:32:51 -0400

    clase arrays

--- Js/EjemplosClase/arr.js
+++ Js/EjemplosClase/arr.js
@@ -1,15 +1,89 @@
-ls
+log = console.log;
+//********************************************************************** */
+log("Ejemplo 1\n\n")
+
+var heroes = ["mago","elfo","mishi","orco"]
+
+log(heroes[1])
+log(heroes[0])
+log(heroes[3])
+log(heroes[2])
+
+//********************************************************************** */
+log("\n\nEjemplo 2\n\n")
+
 let arrayA = [];
-console.log(arrayA);
+log(arrayA);
 
 arrayA.unshift('Daniel')
-console.log(arrayA);
+log(arrayA);
 
 arrayA.push('Venezuela');
-console.log(arrayA);
+log(arrayA);
 
 arrayA.shift();
-console.log(arrayA);
+log(arrayA);
 
 arrayA.pop();
-console.log(arrayA);
+log(arrayA);
+//********************************************************************** */
+log("\n\nEjemplo 3\n\n")
+
+if (heroes[2] == "orco") {
+    log("es Orco\n")
+    
+}else{
+    log("No es orco \n")
+}
+//********************************************************************** */
+log("\n\nEjemplo 4\n\n")
+
+pos = 1
+heroes[pos] = "alien"
+log(heroes[pos])
+log(heroes)
+
+//********************************************************************** */
+log("\n\nEjemplo 5\n\n")
+hero=["mago","elfo","mishi","orco","humano","troll","gargola"]
+for (let index = 1; index <= 5; index++){
+    log(hero[index])
+}
+
+//********************************************************************** */
+log("\n\nEjemplo 6\n\n")
+
+let tree = "🌴"
+let road = "🛣"
+let genius = "🧞‍♂️"
+let house = "🏡"
+
+let map = [
+    ["g", 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, "h"],
+    [0, 0, 0, 0, 0, 0],
+]
+
+for (let fila = 0; fila < map.length; fila++) {
+    let col = 0;
+    for (col = 0; col < 6; col++) {
+        if (map[fila][col] == "g") {
+            map[fila][col] = genius
+        }
+        if (map[fila][col] == 0) {
+            map[fila][col] = tree
+        }
+        if (map[fila][col] == 1) {
+            map[fila][col] = road
+        }
+        if (map[fila][col] == "h") {
+            map[fila][col] = house
+        }
+    }
+}
+
+console.log(map)
